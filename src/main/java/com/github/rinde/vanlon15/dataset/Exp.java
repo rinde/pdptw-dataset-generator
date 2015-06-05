1dd901fd527f455050ade304d668d9a64729f807
Rinde van Lon <rindevanlon@gmail.com>
2015-06-05 09:39:00 +0200

    created experiment class

---
+++ src/main/java/com/github/rinde/vanlon15/dataset/Exp.java
@@ -0,0 +1,156 @@
+/*
+ * Copyright (C) 2011-2015 Rinde van Lon, iMinds-DistriNet, KU Leuven
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.rinde.vanlon15.dataset;
+
+import static java.util.Arrays.asList;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import com.github.rinde.logistics.pdptw.solver.CheapestInsertionHeuristic;
+import com.github.rinde.rinsim.central.Central;
+import com.github.rinde.rinsim.experiment.CommandLineProgress;
+import com.github.rinde.rinsim.experiment.Experiment;
+import com.github.rinde.rinsim.experiment.Experiment.SimulationResult;
+import com.github.rinde.rinsim.experiment.ExperimentResults;
+import com.github.rinde.rinsim.experiment.MASConfiguration;
+import com.github.rinde.rinsim.io.FileProvider;
+import com.github.rinde.rinsim.pdptw.common.AddVehicleEvent;
+import com.github.rinde.rinsim.scenario.gendreau06.Gendreau06ObjectiveFunction;
+import com.google.common.base.Charsets;
+import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
+import com.google.common.collect.FluentIterable;
+import com.google.common.collect.LinkedHashMultimap;
+import com.google.common.collect.Multimap;
+import com.google.common.io.Files;
+
+/**
+ * @author Rinde van Lon
+ *
+ */
+public class Exp {
+  static final Gendreau06ObjectiveFunction SUM = Gendreau06ObjectiveFunction
+    .instance();
+
+  static final String DATASET = "files/dataset/";
+  static final String RESULTS = "files/results/";
+
+  public static void main(String[] args) {
+
+    // FileProvider.builder()
+    // .add(Paths.get(DATASET))
+    // .filter("glob:**.scen")
+
+    final long time = System.currentTimeMillis();
+    final Experiment.Builder experimentBuilder = Experiment
+      .build(SUM)
+      .computeLocal()
+      // .computeDistributed()
+      .withRandomSeed(123)
+      .repeat(1)
+      .numBatches(10)
+      .addScenarios(FileProvider.builder()
+        .add(Paths.get(DATASET))
+        .filter("glob:**-1.00-0.scen")
+      )
+      .addResultListener(new CommandLineProgress(System.out))
+      .addConfiguration(Central.solverConfiguration(
+        CheapestInsertionHeuristic.supplier(SUM), "-CheapInsert"));
+
+    final ExperimentResults results = experimentBuilder.perform();
+    final long duration = System.currentTimeMillis() - time;
+    System.out.println("Done, computed " + results.results.size()
+      + " simulations in " + duration / 1000d + "s");
+
+    final Multimap<MASConfiguration, SimulationResult> groupedResults = LinkedHashMultimap
+      .create();
+    for (final SimulationResult sr : results.sortedResults()) {
+      groupedResults.put(sr.masConfiguration, sr);
+    }
+
+    for (final MASConfiguration config : groupedResults.keySet()) {
+      final Collection<SimulationResult> group = groupedResults.get(config);
+
+      final File configResult = new File(RESULTS + config.getName()
+        + ".csv");
+      try {
+        Files.createParentDirs(configResult);
+      } catch (final IOException e1) {
+        throw new IllegalStateException(e1);
+      }
+      // deletes the file in case it already exists
+      configResult.delete();
+      try {
+        Files
+          .append(
+            "dynamism,urgency,scale,cost,travel_time,tardiness,over_time,is_valid,scenario_id,random_seed,comp_time,num_vehicles\n",
+            configResult,
+            Charsets.UTF_8);
+      } catch (final IOException e1) {
+        throw new IllegalStateException(e1);
+      }
+
+      for (final SimulationResult sr : group) {
+        final String pc = sr.scenario.getProblemClass().getId();
+        final String id = sr.scenario.getProblemInstanceId();
+        final int numVehicles = FluentIterable.from(sr.scenario.getEvents())
+          .filter(AddVehicleEvent.class).size();
+        try {
+          final String scenarioName = Joiner.on("-").join(pc, id);
+          final List<String> propsStrings = Files.readLines(new File(
+            DATASET + scenarioName + ".properties"),
+            Charsets.UTF_8);
+          final Map<String, String> properties = Splitter.on("\n")
+            .withKeyValueSeparator(" = ")
+            .split(Joiner.on("\n").join(propsStrings));
+
+          final double dynamism = Double.parseDouble(properties
+            .get("dynamism_bin"));
+          final long urgencyMean = Long.parseLong(properties.get("urgency"));
+          final double scale = Double.parseDouble(properties.get("scale"));
+
+          final double cost = SUM.computeCost(sr.stats);
+          final double travelTime = SUM.travelTime(sr.stats);
+          final double tardiness = SUM.tardiness(sr.stats);
+          final double overTime = SUM.overTime(sr.stats);
+          final boolean isValidResult = SUM.isValidResult(sr.stats);
+          final long computationTime = sr.stats.computationTime;
+
+          final String line = Joiner.on(",")
+            .appendTo(new StringBuilder(),
+              asList(dynamism, urgencyMean, scale, cost, travelTime,
+                tardiness, overTime, isValidResult, scenarioName, sr.seed,
+                computationTime, numVehicles))
+            .append(System.lineSeparator())
+            .toString();
+          if (!isValidResult) {
+            System.err.println("WARNING: FOUND AN INVALID RESULT: ");
+            System.err.println(line);
+          }
+          Files.append(line, configResult, Charsets.UTF_8);
+        } catch (final IOException e) {
+          throw new IllegalStateException(e);
+        }
+      }
+    }
+
+  }
+}
